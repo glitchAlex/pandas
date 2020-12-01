2d8b78f6018a2966f541ad578f62ac4b6190794b
glitchAlex <74685665+glitchAlex@users.noreply.github.com>
2020-12-01 20:16:32 +0300

    Episode 1, pandas

---
+++ lab_pd_1.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+#input
+file_path = 'C:\DEV\\transactions.csv'
+df = pd.read_csv(file_path, index_col=0)
+
+#main
+print(df.loc[df['STATUS'] == 'OK'].sort_values(by = 'SUM', ascending = False)[0:3])
+
+print('===================================')
+
+print('Umbrella, Inc transaction summary: ', df['SUM'][df['CONTRACTOR'] == 'Umbrella, Inc'][df['STATUS'] == 'OK'].sum())
+
+print('\nturtle')
